79efb33346c2b4dae29bddd17a92d8ac75d69f0b
cz <lchu976@gmail.com>
2020-10-30 17:04:56 -0400

    first commit

---
+++ server/config/db.js
@@ -0,0 +1,6 @@
+module.exports = {
+  //local MongoDB deployment ->
+  //"URI": "mongodb://localhost/books229"
+
+  "URI":"mongodb+srv://cz:JHXx43lwOZYfgIlH@mongodbsever.mnhik.mongodb.net/books229?retryWrites=true&w=majority"
+};
